ae953b77de0f8883d94c2a179fbca7e4acae05bd
pksatyam777 <pksatyam777@gmail.com>
2023-03-13 15:47:24 +0530

    Updates

---
+++ precache-manifest.a426584853ca70b5479c4370d44d6a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ae197f2a262297a7e79",
+    "url": "/pksatyam777/Food-Order-App/static/css/main.862223a2.chunk.css"
+  },
+  {
+    "revision": "5ae197f2a262297a7e79",
+    "url": "/pksatyam777/Food-Order-App/static/js/main.5ae197f2.chunk.js"
+  },
+  {
+    "revision": "330c678f2193ea1e5f8d",
+    "url": "/pksatyam777/Food-Order-App/static/js/1.330c678f.chunk.js"
+  },
+  {
+    "revision": "55b770223332e20fa415",
+    "url": "/pksatyam777/Food-Order-App/static/js/runtime~main.55b77022.js"
+  },
+  {
+    "revision": "7ebd6123cfcffad67dd4843fee7962a1",
+    "url": "/pksatyam777/Food-Order-App/static/media/food-cover.7ebd6123.jpg"
+  },
+  {
+    "revision": "757cf8a3b461c60adf81d2eee3d820fa",
+    "url": "/pksatyam777/Food-Order-App/static/media/logo.757cf8a3.svg"
+  },
+  {
+    "revision": "ebccea19eb5cf238cd740d113ef59f27",
+    "url": "/pksatyam777/Food-Order-App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pksatyam777/Food-Order-App/precache-manifest.a426584853ca70b5479c4370d44d6a30.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pksatyam777/Food-Order-App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
